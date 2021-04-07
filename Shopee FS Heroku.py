0cf83a0afa93367adf845d3739a96982a8c7947a
Gitaprastio <54318397+Gitaprastio@users.noreply.github.com>
2021-04-08 01:13:13 +0700

    2

--- Shopee FS Heroku.py
+++ Shopee FS Heroku.py
@@ -25,9 +25,16 @@ skipTimer = False
 repeat = True
 AutoTime = True
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
 #AutoScheduler on Progress
 if AutoTime == True:
-    driver = webdriver.Firefox(options=optionss)
+    driver = webdriver.Chrome(options=optionss)
     driver.get(urlpage)
     time.sleep(3)
     sessionPointer = driver.find_elements_by_xpath("//*[@class='flash-sale-session__display-hour']")
